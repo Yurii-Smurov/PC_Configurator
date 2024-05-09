f36fdd9beb8ad0c2c2c208aa45fd703b3f53b252
bebrios <yurij.smurov27@gmail.com>
2024-05-09 17:39:36 +0700

    Добавлены классы для моделей систем охлаждения процессора

---
+++ Configurator/Configurator/Models/Components/AirCoolingSystem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Configurator.Models.Components
+{
+    class AirCoolingSystem : CoolingSystem
+    {
+        public AirCoolingSystem(string name, decimal price, string manufacturer, int stock) : base(name, price, manufacturer, stock)
+        {
+        }
+    }
+}
---
+++ Configurator/Configurator/Views/ViewController.cs
@@ -0,0 +1,29 @@
+﻿using Configurator.Views.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Configurator.Views
+{
+    class ViewController
+    {
+        private IView _currentView;
+
+        public ViewController()
+        {
+            _currentView = new RegistrationView(this);
+        }
+
+        public void ChangeState(IView view)
+        {
+            _currentView = view;
+        }
+
+        public void ShowCurrentView()
+        {
+            _currentView.Show();
+        }
+    }
+}
